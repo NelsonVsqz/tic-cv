ee70ef20379aa9f9e4972059fe81b69d2ce436bd
Nelson Vasquez <nelson10846@gmail.com>
2023-02-25 04:42:25 -0300

    presentar

--- cv.js
+++ cv.js
@@ -5,24 +5,24 @@ let experiencia = document.getElementById("experienciaboton");
 let habilidades = document.getElementById("habilidadesboton");
 
 // Obtener las secciones a ocultar y mostrar
-let infopersonalSeccion =document.getElementById("infopersonalseccion");
-let educacionSeccion = document.getElementById("educacionSeccion");
-let experienciaSeccion = document.getElementById("experienciaSeccion");
-let habilidadesSeccion = document.getElementById("habilidadesSeccion");
+let infopersonalSeccion = document.getElementById("Informacion");
+let educacionSeccion = document.getElementById("Educacion");
+let experienciaSeccion = document.getElementById("Experiencia");
+let habilidadesSeccion = document.getElementById("Habilidades");
 
 // Añadir eventos de click a los botones
-info.addEventListener("click", function() {
+info.addEventListener("click", function () {
   verSeccion(infopersonalSeccion);
 });
-educacion.addEventListener("click", function() {
+educacion.addEventListener("click", function () {
   verSeccion(educacionSeccion);
 });
 
-experiencia.addEventListener("click", function() {
+experiencia.addEventListener("click", function () {
   verSeccion(experienciaSeccion);
 });
 
-habilidades.addEventListener("click", function() {
+habilidades.addEventListener("click", function () {
   verSeccion(habilidadesSeccion);
 });
 
@@ -37,70 +37,70 @@ function verSeccion(section) {
 //Nav
 let sections = document.querySelectorAll("section");
 let nav = document.getElementById("nav");
-
 // Crear un enlace para cada sección
-sections.forEach(function(section) {
+sections.forEach(function (section) {
 
   let link = document.createElement("a");
   link.href = "#" + section.id;
   link.innerHTML = section.id;
-  
-  link.addEventListener("click", function() {
+
+  link.addEventListener("click", function () {
     // Ocultar todas las secciones al hacer clic en un enlace
-    sections.forEach(function(section) {
+    sections.forEach(function (section) {
       section.style.display = "none";
     });
-    
     // Mostrar solo la sección correspondiente al enlace clickeado
     document.getElementById(link.innerHTML).style.display = "block";
   });
-  
+
   nav.appendChild(link);
 });
 
 
+
+
 //Función para agregar un nuevo campo a una sección específica
-function addField(sectionId, fieldName, fieldValue,fieldValue2) {
+function addField(sectionId, fieldName, fieldValue, fieldValue2) {
   var section = document.getElementById(sectionId);
-  switch(section.id){
-  case "infopersonalseccion":  
-  var newField = document.createElement("div");
-  newField.innerHTML = "<p>" + fieldName + ":"+ fieldValue+"</p> ";
-  section.appendChild(newField);
-  break
-  case "educacionSeccion":  
-  var newField = document.createElement("div");
-  newField.innerHTML = "<h4 id="+fieldName+" >" + fieldName +"</h4> ";
-  section.appendChild(newField);
-  var newp = document.createElement("p");
-  var fieldN = document.getElementById(fieldName);
-  newp.innerHTML = "<p>"+ fieldValue +"</p> ";
-  section.appendChild(newp);  
-  break
-  case "experienciaSeccion":  
-  var newField = document.createElement("div");
-  newField.innerHTML = "<h4 id="+fieldName+" >" + fieldName +"</h4> ";
-  section.appendChild(newField);
-  var newp = document.createElement("p");
-  var fieldN = document.getElementById(fieldName);
-  newp.innerHTML = "<p>"+ fieldValue +"</p> ";
-  section.appendChild(newp); 
-  var newp2 = document.createElement("p");
-  newp2.innerHTML = "<p>"+ fieldValue2 +"</p> ";
-  section.appendChild(newp2); 
+  switch (section.id) {
+    case "Informacion":
+      var newField = document.createElement("div");
+      newField.innerHTML = "<p>" + fieldName + ": " + fieldValue + "</p> ";
+      section.appendChild(newField);
+      break
+    case "Educacion":
+      var newField = document.createElement("div");
+      newField.innerHTML = "<h4 id=" + fieldName + " >" + fieldName + "</h4> ";
+      section.appendChild(newField);
+      var newp = document.createElement("p");
+      var fieldN = document.getElementById(fieldName);
+      newp.innerHTML = "<p>" + fieldValue + "</p> ";
+      section.appendChild(newp);
+      break
+    case "Experiencia":
+      var newField = document.createElement("div");
+      newField.innerHTML = "<h4 id=" + fieldName + " >" + fieldName + "</h4> ";
+      section.appendChild(newField);
+      var newp = document.createElement("p");
+      var fieldN = document.getElementById(fieldName);
+      newp.innerHTML = "<p>" + fieldValue + "</p> ";
+      section.appendChild(newp);
+      var newp2 = document.createElement("p");
+      newp2.innerHTML = "<p>" + fieldValue2 + "</p> ";
+      section.appendChild(newp2);
 
-  break
-  case "habilidadesSeccion":  
-  var lista = document.getElementById("lista"); 
-  var newField = document.createElement("li");
-  var nuevo = document.createTextNode(fieldName);
-  newField.appendChild(nuevo);
-  lista.appendChild(newField);
+      break
+    case "Habilidades":
+      var lista = document.getElementById("lista");
+      var newField = document.createElement("li");
+      var nuevo = document.createTextNode(fieldName);
+      newField.appendChild(nuevo);
+      lista.appendChild(newField);
 
-  break
+      break
 
-  default:
-  break
+    default:
+      break
   }
 
 }
@@ -108,69 +108,69 @@ function addField(sectionId, fieldName, fieldValue,fieldValue2) {
 //Función para modificar el valor de un campo existente en una sección específica
 function editField(sectionId, fieldName, newValue, fieldName2, newValue2, fieldName3, newValue3) {
   var section = document.getElementById(sectionId);
-  switch(section.id){
-  case "infopersonalseccion":    
-  var fields = section.getElementsByTagName("p");
-  for(var i = 0; i < fields.length; i++) {
-    if(fields[i].innerHTML.indexOf(fieldName) !== -1) {
-      fields[i].innerHTML = "<p>" + newValue + ":</p> ";
-      break;
-    }
-  }
-  break
-  case "educacionSeccion":
-    var fields = section.getElementsByTagName("h4");
-    for(var i = 0; i < fields.length; i++) {
-      if(fields[i].innerHTML.indexOf(fieldName) !== -1) {
-        fields[i].innerHTML = "<h4>" + newValue + "</h4>";
-        break;
+  switch (section.id) {
+    case "Informacion":
+      var fields = section.getElementsByTagName("p");
+      for (var i = 0; i < fields.length; i++) {
+        if (fields[i].innerHTML.indexOf(fieldName) !== -1) {
+          fields[i].innerHTML = "<p>" + newValue + "</p> ";
+          break;
+        }
       }
-    }
-  var fields = section.getElementsByTagName("p");
-    for(var i = 0; i < fields.length; i++) {
-      if(fields[i].innerHTML.indexOf(fieldName2) !== -1) {
-        fields[i].innerHTML = "<p>" + newValue2 + "</p> ";
-        break;
+      break
+    case "educacionSeccion":
+      var fields = section.getElementsByTagName("h4");
+      for (var i = 0; i < fields.length; i++) {
+        if (fields[i].innerHTML.indexOf(fieldName) !== -1) {
+          fields[i].innerHTML = "<h4>" + newValue + "</h4>";
+          break;
+        }
       }
-    }
+      var fields = section.getElementsByTagName("p");
+      for (var i = 0; i < fields.length; i++) {
+        if (fields[i].innerHTML.indexOf(fieldName2) !== -1) {
+          fields[i].innerHTML = "<p>" + newValue2 + "</p> ";
+          break;
+        }
+      }
 
-  break
-  case "experienciaSeccion":
-    var fields = section.getElementsByTagName("h4");
-    for(var i = 0; i < fields.length; i++) {
-      if(fields[i].innerHTML.indexOf(fieldName) !== -1) {
-        fields[i].innerHTML = "<h4>" + newValue + "</h4>";
-        break;
+      break
+    case "experienciaSeccion":
+      var fields = section.getElementsByTagName("h4");
+      for (var i = 0; i < fields.length; i++) {
+        if (fields[i].innerHTML.indexOf(fieldName) !== -1) {
+          fields[i].innerHTML = "<h4>" + newValue + "</h4>";
+          break;
+        }
       }
-    }
-  var fields = section.getElementsByTagName("p");
-    for(var i = 0; i < fields.length; i++) {
-      if(fields[i].innerHTML.indexOf(fieldName2) !== -1) {
-        fields[i].innerHTML = "<p>" + newValue2 + "</p> ";
-        break;
+      var fields = section.getElementsByTagName("p");
+      for (var i = 0; i < fields.length; i++) {
+        if (fields[i].innerHTML.indexOf(fieldName2) !== -1) {
+          fields[i].innerHTML = "<p>" + newValue2 + "</p> ";
+          break;
+        }
       }
-    }
-    for(var i = 0; i < fields.length; i++) {
-      if(fields[i].innerHTML.indexOf(fieldName3) !== -1) {
-        fields[i].innerHTML = "<p>" + newValue3 + "</p> ";
-        break;
+      for (var i = 0; i < fields.length; i++) {
+        if (fields[i].innerHTML.indexOf(fieldName3) !== -1) {
+          fields[i].innerHTML = "<p>" + newValue3 + "</p> ";
+          break;
+        }
       }
-    }
 
-  break
-  case "habilidadesSeccion":
-    var fields = section.getElementsByTagName("li");
-    for(var i = 0; i < fields.length; i++) {
-      if(fields[i].innerHTML.indexOf(fieldName) !== -1) {
-        fields[i].innerHTML = "<li>" + newValue + "</li>";
-        break;
+      break
+    case "habilidadesSeccion":
+      var fields = section.getElementsByTagName("li");
+      for (var i = 0; i < fields.length; i++) {
+        if (fields[i].innerHTML.indexOf(fieldName) !== -1) {
+          fields[i].innerHTML = "<li>" + newValue + "</li>";
+          break;
+        }
       }
-    }
 
-  break
+      break
 
-  default :
-  break
+    default:
+      break
   }
 }
 
@@ -178,8 +178,8 @@ function editField(sectionId, fieldName, newValue, fieldName2, newValue2, fieldN
 function deleteField(sectionId, fieldName) {
   var section = document.getElementById(sectionId);
   var fields = section.getElementsByTagName("div");
-  for(var i = 0; i < fields.length; i++) {
-    if(fields[i].innerHTML.indexOf(fieldName) !== -1) {
+  for (var i = 0; i < fields.length; i++) {
+    if (fields[i].innerHTML.indexOf(fieldName) !== -1) {
       section.removeChild(fields[i]);
       break;
     }
@@ -199,53 +199,55 @@ const generarUsuario = async () => {
   const data = results[0]
   console.log(data)
 
-foto.src = data.picture.medium
-nombre.textContent = data.name.first
-correo.textContent = data.email
-celu.textContent = data.phone
+  foto.src = data.picture.large
+  nombre.textContent = data.name.first
+  correo.textContent = data.email
+  celu.textContent = data.phone
 
   //return data;
 }
-boton.addEventListener('click',generarUsuario)
-//document.addEventListener('DOMContentLoaded',generarUsuario)
+boton.addEventListener('click', generarUsuario)
+document.addEventListener('DOMContentLoaded', generarUsuario)
 //Validar formulario de contacto
 
-let form = document.getElementById("contactameForm");
-let nombreForm = document.getElementById("nombreForm");
-let emailForm = document.getElementById("emailForm");
-let mensajeForm = document.getElementById("mensajeForm");
+const form = document.querySelector("form");
+const nombreForm = document.getElementById("nombreForm");
+const emailForm = document.getElementById("emailForm");
+const mensajeForm = document.getElementById("mensajeForm");
 
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  if (validateForm()) {
-    alert("Formulario enviado correctamente");
-    form.reset();
+form.addEventListener("submit", function(event) {
+  if (!nombreForm.value) {
+    nombreForm.classList.add("is-invalid");
+    event.preventDefault();
+  } else {
+    nombreForm.classList.remove("is-invalid");
+  }
+
+  if (!emailForm.value) {
+    emailForm.classList.add("is-invalid");
+    event.preventDefault();
+  } else {
+    emailForm.classList.remove("is-invalid");
+  }
+
+  if (!mensajeForm.value) {
+    mensajeForm.classList.add("is-invalid");
+    event.preventDefault();
+  } else {
+    mensajeForm.classList.remove("is-invalid");
   }
 });
 
-function validateForm() {
-  let isValid = true;
+function showButtons(event, id) {
+  const buttonContainer = document.querySelector(`#button-container2-${id}`);
+  const x = event.clientX;
+  const y = event.clientY;
+  buttonContainer.style.display = 'block';
+  buttonContainer.style.top = y + 'px';
+  buttonContainer.style.left = x + 'px';
+}
 
-  if (name.value === "") {
-    name.classList.add("error");
-    isValid = false;
-  } else {
-    name.classList.remove("error");
-  }
-
-  if (email.value === "") {
-    email.classList.add("error");
-    isValid = false;
-  } else {
-    email.classList.remove("error");
-  }
-
-  if (message.value === "") {
-    message.classList.add("error");
-    isValid = false;
-  } else {
-    message.classList.remove("error");
-  }
-
-  return isValid;
+function hideButtons(id) {
+  const buttonContainer = document.querySelector(`#button-container2-${id}`);
+  buttonContainer.style.display = 'none';
 }
